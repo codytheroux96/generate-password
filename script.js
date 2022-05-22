c174c1a1dbbe2a52d1b714ce75dfdf503f605e4d
cdt96 <cody.theroux3@gmail.com>
2022-05-21 23:35:36 -0400

    made a variable for what the user inputs for length of password and made an if statement for if they dont put a number in for it.

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
- var Input= prompt("Are you ready for a password?")
+ var Input= prompt("How many characters do you want in your password?")
+var length = parseInt(Input)
+if (isNaN(length)){
+  alert("That's not a number.")
+}
 }
 // Write password to the #password input
 function writePassword() {
