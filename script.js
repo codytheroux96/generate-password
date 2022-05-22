9e8dacac6fa6a07fdf22e6b02dbc2fa059969ceb
cdt96 <cody.theroux3@gmail.com>
2022-05-21 23:40:23 -0400

    added comments

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  //when the user clicks on the generate password button they will be prompted with the question "how many characters do you want in your password?"
  var Input= prompt("How many characters do you want in your password?")
+ //Assigning a variable to the information that the user inputs for how long they want their password to be
 var length = parseInt(Input)
+//if the user inputs anything that is not a number it will prompt them with this message
 if (isNaN(length)){
   alert("That's not a number.")
 }
